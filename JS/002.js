212264c893b6261c447639a0c24c45b364a8ae0e
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-11-26 16:05:19 +0900

    21-11-26

---
+++ JS/002.js
@@ -0,0 +1,31 @@
+// https://ko.javascript.info/ 소개
+// https://www.w3schools.com/ 소개
+
+// 자바스크립트로 출력하는 방법
+// let age = 10; //지금은 다루지 않습니다.
+// const pi = 3.141592; // 지금은 다루지 않습니다.
+// var user = "사용자";
+// var age = 20;
+
+// document.getElementById("title").innerHTML = "Welcome to JS100제";
+// document.write("20 + 20");
+// window.alert(user);
+// console.log(age);
+// 한 줄 주석
+/* 주석
+  입니다. */
+
+var myVal1 = "kokoko";
+var myVal2 = 324234;
+var myVal3 = [1, 2, 3, 4, 5, 6];
+var myVal4 = { val1: 1, val: 2 };
+var myval5 = function () {};
+
+var myVal6 = [1, 2, 3],
+  myVal7 = [4, 5, 6];
+
+var myVal8 = "조은 선생님";
+var MyVal8 = "호준 선생님";
+
+window.alert(myVal8);
+window.alert(MyVal8);
