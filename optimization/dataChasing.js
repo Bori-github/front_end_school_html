ddf9af6ce0c85df1d5004c4f28008f88a0c03025
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-12-20 16:55:06 +0900

    21-12-20

---
+++ optimization/dataChasing.js
@@ -0,0 +1,12 @@
+const aespa = ["카리나", "윈터", "지젤", "닝닝"];
+
+// aespa.forEach((item, index) => {
+//   aespa[index] = item + "♥";
+// });
+
+const aespa2 = aespa.map((item) => {
+  return item + "♥";
+});
+
+console.log(aespa);
+console.log(aespa2);
