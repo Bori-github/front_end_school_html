db130eb336b59fa7de11267258f5c748da9eb426
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-12-23 16:16:10 +0900

    21-12-23

---
+++ node/004/app.js
@@ -0,0 +1,9 @@
+import {이름, 나이, 한살더먹음, 값가져와} form './two.js'
+// const 개인프로필 = require("./two");
+
+console.log(개인프로필.이름);
+console.log(개인프로필.나이);
+개인프로필.한살더먹음();
+개인프로필.한살더먹음();
+console.log(개인프로필.나이);
+console.log(개인프로필.값가져오기());
---
+++ node/004/two.js
@@ -0,0 +1,9 @@
+exports let 이름 = "보리";
+exports let 나이 = 30;
+exports function 한살더먹음() {
+  나이 += 1;
+}
+exports function 값가져오기() {
+  return 나이;
+}
+
