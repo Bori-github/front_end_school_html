4da73f7b90f98b5cea230dca729ca89eb653b770
Bori-github <85009583+Bori-github@users.noreply.github.com>
2022-01-27 22:19:48 +0900

    22-01-27

---
+++ node/011_blog_api/blog.js
@@ -0,0 +1,32 @@
+const nunjuks = require("nunjuks");
+const express = require("express");
+
+let blog = [
+  {
+    id: 1,
+    title: "title",
+    content: "content1",
+    section: "section1",
+    pubDate: new Date.toString(),
+    modDate: new Date.toString(),
+    viewCount: 0,
+  },
+  {
+    id: 1,
+    title: "title",
+    content: "content1",
+    section: "section1",
+    pubDate: new Date.toString(),
+    modDate: new Date.toString(),
+    viewCount: 0,
+  },
+  {
+    id: 1,
+    title: "title",
+    content: "content1",
+    section: "section1",
+    pubDate: new Date.toString(),
+    modDate: new Date.toString(),
+    viewCount: 0,
+  },
+];
